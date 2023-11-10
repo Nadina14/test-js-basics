76b1b61fdc4734eb957ad8cdf6a20101b2e77b79
Nadina <sheriefnadina@gmail.com>
2023-11-10 12:18:44 +0100

    feat/ correzione es-1

--- es-1/main.js
+++ es-1/main.js
@@ -1,19 +1,23 @@
 /* Manipolazione di stringhe e numeri */
 
-function analizzaTesto(testo){
-    const originale = testo;
-    const minuscolo = testo.toLowerCase();
-    const senzaSpazi = testo.trim();
-    const numeroCaratteri = testo.length(); 
-    const contieneJS = testo.includes('JS' || 'JavaScript');
-    let giraParola = (x) => {
-       let parolaGirata = '';
-       for (let i = x.length - 1; i >= 0; i--){
-       parolaGirata += x[i]; 
-       }
-       return parolaGirata;
-    };
-    giraParola(); 
-    return originale, minuscolo, senzaSpazi, numeroCaratteri, contieneJS;
+function analizzaTesto(testo) {
+    const risultato = {
+        originale: testo,
+        minuscolo: testo.toLowerCase(),
+        senzaSpazi: testo.trim(),
+        numeroCaratteri: testo.length,
+        contieneJS: testo.includes('JS' || 'JavaScript'),
+        invertito: giraParola(testo)
+    }
+    return risultato;
 }
-analizzaTesto('JS mi fa venire gli incubi');
+
+const giraParola = (x) => {
+    let parolaGirata = '';
+    for (let i = x.length - 1; i >= 0; i--) {
+        parolaGirata += x[i];
+    }
+    return parolaGirata;
+};
+/* let fnc = analizzaTesto('JS mi fa venire gli incubi');
+console.log(fnc) */
