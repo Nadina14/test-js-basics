a8493325b5cd363b7ab741a2a02a0e559a8ac0e9
Nadina <sheriefnadina@gmail.com>
2023-11-10 11:33:51 +0100

    feat/ es-2

---
+++ es-2/main.js
@@ -0,0 +1,29 @@
+/* Funzioni, Scoping e Cicli */
+
+function riempiArray(numeroElementi, valoreMinimo, valoreMassimo){
+    let array = [];
+    for(i = 0; i < numeroElementi; i++){
+        let n = (Math.floor(Math.random() * (valoreMassimo - valoreMinimo + 1)) + valoreMinimo);
+        array.push(n);
+    };
+    return array;
+}
+let arrayCasuale = riempiArray(10, 2, 50);
+console.log(arrayCasuale); 
+
+let contaPariDispari = (array) => {
+        let obj = {
+            pari: 0,
+            dispari: 0
+        }
+        for(j = 0; j < array.length; j++){
+            if (array[j] % 2 == 0){
+               obj.pari ++;
+            }else {
+            obj.dispari ++;
+            }
+        }
+        return obj ;
+    }
+let conteggio = contaPariDispari(arrayCasuale);
+console.log(conteggio);
