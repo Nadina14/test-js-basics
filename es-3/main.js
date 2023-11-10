dbcec02e72ad58dea26680b3a814ac12d1c73de7
Nadina <sheriefnadina@gmail.com>
2023-11-10 12:57:39 +0100

    feat/ bonus-tentativo

--- es-3/main.js
+++ es-3/main.js
@@ -11,4 +11,39 @@ window.addEventListener('load', () => {
             alert('Inserisci una parola prima di inviare!');
         }
     })
-})
+}) 
+
+/* BONUS */
+/* window.addEventListener('load', () => {
+    const btn = document.getElementById('bottoneInvio');
+    btn.addEventListener('click', () => {
+        function analizzaTesto(testo) {
+            const risultato = {
+                originale: testo,
+                minuscolo: testo.toLowerCase(),
+                senzaSpazi: testo.trim(),
+                numeroCaratteri: testo.length,
+                contieneJS: testo.includes('JS' || 'JavaScript'),
+                invertito: giraParola(testo)
+            }
+            return risultato;
+        }
+        const giraParola = (x) => {
+            let parolaGirata = '';
+            for (let i = x.length - 1; i >= 0; i--) {
+                parolaGirata += x[i];
+            }
+            return parolaGirata;
+        };
+        const input = document.getElementById('testoUtente');
+        const inputUtente = input.value;
+        const r = analizzaTesto(inputUtente);
+        console.log(r);
+        let p = document.getElementById('risultato');
+        if(input.value != ''){
+            document.body.innerHTML += `<ul><li>${Object.keys(risultato) , Object.values(risultato)}</li><li>${Object.keys(risultato) , Object.values(risultato)}</li><li>${Object.keys(risultato) , Object.values(risultato)}</li></ul>`;
+        }else {
+            alert('Inserisci una parola prima di inviare!');
+        }
+    })
+}) */
