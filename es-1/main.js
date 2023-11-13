ef23dc22c12abca2757624d5ff727eab16b00dae
Nadina <sheriefnadina@gmail.com>
2023-11-13 11:04:15 +0100

    correzione es.1

--- es-1/main.js
+++ es-1/main.js
@@ -6,7 +6,7 @@ const analizzaTesto = (testo) => {
     obj.minuscolo = testo.toLowerCase();
     obj.senzaSpazi = testo.trim();
     obj.numeroCaratteri = obj.senzaSpazi.length;
-    obj.contieneJS = testo.includes('JS' || 'JavaScript');
+    obj.contieneJS = testo.includes('JS') || testo.includes('JavaScript');
     obj.invertita = '';
     for (let i = testo.length - 1; i >= 0; i--) {
         const char = testo[i];
